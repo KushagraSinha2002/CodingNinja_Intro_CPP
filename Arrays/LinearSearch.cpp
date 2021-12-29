61fcbbef695b07cab4f6b23be49b96e8c58ae9b6
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-12-29 12:59:14 +0530

    Array

---
+++ Arrays/LinearSearch.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int *arr, int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            return i;
+        }
+        
+    }
+    return -1;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int *arr = new int[n];
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> arr[i];
+		}
+		int val;
+		cin >> val;
+		cout << linearSearch(arr, n, val) << endl;
+	}
+	return 0;
+}
