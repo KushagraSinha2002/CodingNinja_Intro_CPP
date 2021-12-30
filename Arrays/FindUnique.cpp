b7cff3b6bca766ac84a370c16de3423d0bd93564
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2021-12-30 11:43:53 +0530

    Array

---
+++ Arrays/FindUnique.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int findUnique(int *arr, int size)
+{
+    int count;
+    for (int i = 0; i < size; i++)
+    {
+        count = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        if(count == 1){
+            return arr[i];
+        }
+    }
+}
+
+int main()
+{
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int size;
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; ++i)
+		{
+			cin >> input[i];
+		}
+
+		cout << findUnique(input, size) << endl;
+	}
+
+	return 0;
+}
